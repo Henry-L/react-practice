17da1dd309e579c9518db675d932d7b7532857db
Henry Lee <henrylee423@gmail.com>
2016-12-18 16:37:18 -0500

    add parent level App module to house the previous modules

---
+++ js/modules/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import InformationList from './InformationList'
+import DataView from './DataView'
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedObject: { app_keys: [] }
+        };
+
+        this.selectObject = this.selectObject.bind(this);
+    }
+
+    selectObject(selectedObject) {
+        this.setState({
+            selectedObject: selectedObject
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={{ float: 'left' }}>
+                    <InformationList
+                        products={this.props.products}
+                        selectedObject={this.state.selectedObject}
+                        onObjectSelect={this.selectObject}
+                    />
+                </div>
+                <div style={{ float: 'right' }}>
+                    <DataView
+                        products={this.props.products}
+                        selectedObject={this.state.selectedObject}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App
